8ec234e0c8e5ccabe1435f82a5ae0d622789232f
mrcruz117 <mrcruz1989@gmail.com>
2022-04-20 11:12:13 -0400

    Saving info to txt file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password},\n")
+
+    website_entry.delete(0,END)
+    password_entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,14 +35,16 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=43)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=43)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(END, "mrcruz117@icloud.com")
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=30)
+add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 window.mainloop()
