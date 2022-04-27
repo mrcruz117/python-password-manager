961632cf63f99d66f3793c5099d1ebf6e4472975
mrcruz117 <mrcruz1989@gmail.com>
2022-04-27 17:11:04 -0400

    handling errors

--- main.py
+++ main.py
@@ -42,16 +42,22 @@ def save():
         messagebox.showinfo(title="Warning", message="Please fill all fields.")
 
     else:
-        with open("data.json", "r") as data_file:
-            # reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json","w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
-            print(data)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
